0d6be55a201fb0b8fbb0624930641cda0ce681cb
soatra <johnsoatra@gmail.com>
2024-04-29 11:56:49 +0700

    parts

---
+++ components/part/SpaceBottom.tsx
@@ -0,0 +1,12 @@
+import Class from "@/constants/classes";
+import React from "react";
+
+const SpaceBottom = () => {
+    return (
+        <div
+			className={Class.Section}>
+		</div>
+    );
+};
+
+export default SpaceBottom;
