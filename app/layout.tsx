83d9ac8c4a007bf2df80e3747a701d71720aa421
soatra <johnsoatra@gmail.com>
2024-04-29 11:56:38 +0700

    page and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,37 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata, Viewport } from "next";
+import "@/assets/styles/global.scss";
+import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
-    title: "Portfolio",
-    description: "Portfolio of John Soatra",
+    title: "Soatra",
+    description: `
+        John Soatra is a creative software engineer that can build very awesome application.
+        He's very good at math, so he always embed his knowledge with his creativeness into engineering to make a prefect result.
+        He is a kind of person that never give up easily so he is very trustable because when he meets problem,
+        he can stay with it all the time however find the best to solve that problem.
+        You will like Soatra, if you know him.
+    `.trim().replace(/\s+/g, ' '),
+    authors: {
+        name: 'John Soatra',
+        url: 'https://www.soatra.com'
+    },
+    metadataBase: new URL('https://www.soatra.com')
 };
+
+export const viewport: Viewport = {
+    themeColor: 'black',
+    colorScheme: 'dark'
+}
 
 export default function RootLayout({ children }: Readonly<{children: React.ReactNode}>) {
     return (
         <html lang="en">
+            <head>
+                <link rel="preload" href="/pattern.webp" />
+            </head>
             <body>
                 {children}
+                <Toaster />
             </body>
         </html>
     );
---
+++ components/layout/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div
+            className='
+                w-full flex justify-center
+            '>
+            <p
+                className='
+                    text-[10px] text-center
+                '>
+                This portfolio is updated in 2024.
+            </p>
+        </div>
+    );
+}
+
+export default Footer;
