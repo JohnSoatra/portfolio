2fb0255bbbda91cc6f394bb8d61be13666c09c67
soatra <johnsoatra@gmail.com>
2024-04-30 20:47:35 +0700

    layout

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,3 +1,5 @@
+import Vars from '@/constants/vars';
+import Any from '@soatra/any';
 import React from 'react';
 
 const Footer = () => {
@@ -6,12 +8,21 @@ const Footer = () => {
             className='
                 w-full flex justify-center
             '>
-            <p
+            <Any
+                as='p'
                 className='
                     text-[10px] text-center
-                '>
+                '
+                from="opacity-0"
+                to={[
+                    {
+                        state: 'opacity-100',
+                        duration: Vars.Duration,
+                        easing: Vars.Easing,
+                    }
+                ]}>
                 This portfolio is updated in 2024.
-            </p>
+            </Any>
         </div>
     );
 }
