e214a96e44a73ef9ec0ee92d0d2d5313dabc85a5
John Soatra <johnsoatra@gmail.com>
2025-01-15 20:52:45 +0700

    bg

--- components/layout/Background.tsx
+++ components/layout/Background.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Background = () => {
   return (
-    <div className="background fixed top-0 left-0 w-full h-full opacity-80 blur-[10px]" />
+    <div className="background fixed top-0 left-0 w-full h-full opacity-75 blur-[10px]" />
   );
 }
 
