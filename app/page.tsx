26391932b69cc7cb9bee1728a5efbd397c808744
Soatra Loma <john.soatra@lomatechnology.com>
2025-01-15 15:53:46 +0700

    add airplane

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "@/components/home/Hero";
 import Desc from "@/components/home/Bio";
+import Link from "next/link";
+import Image from "next/image";
+import Airplane from "@/components/common/Airplane";
 
 const Home = () => {
     return (
         <div className="w-full min-h-screen flex flex-col items-center overflow-hidden">
-            <div className="flex-1 flex flex-col justify-center items-center mb-[190px]">
-                <div className="flex flex-col justify-center gap-y-[10px] rounded-full aspect-square p-[40px]">
+            <div className="w-full min-h-full max-w-[1080px] relative flex-1 flex flex-col justify-center items-center pb-[190px]">
+                <Airplane />
+                <div className="z-10 flex flex-col justify-center items-center gap-y-[10px] rounded-full aspect-square p-[40px]">
                     {/* <Hero /> */}
                     <div className="flex flex-col items-center text-center text-[32px] md:text-[42px] xl:text-[52px]">
                         <h1 className="tracking-wider font-medium ">John Soatra</h1>
@@ -17,6 +21,10 @@ const Home = () => {
                         {/* <h1 className="tracking-wider font-medium">John Soatra</h1> */}
                         <h2 className="tracking-wider ">software engineer</h2>
                     </div>
+                    <Link href="https://www.linkedin.com/in/soatra/" className="w-[24px] h-[24px] absolute top-5 left-5 opacity-85" target="_blank">
+                        <Image src='/icons/linkedin.png' width={24} height={24} alt="linkedin" />
+                    </Link>
+                    {/* <p>I know you need time to think, but now you already spend it all.</p> */}
                 </div>
             </div>
         </div>
--- components/home/Bio.tsx
+++ components/home/Bio.tsx
@@ -5,9 +5,8 @@ const Desc = () => {
 		<div className="flex justify-center">
 			<div className="flex justify-center">
 				<p className="text-center text-[16px] md:text-[20px] xl:text-[22px] ">
-					I am a lucky boy<br />
-					so you are also lucky<br />
-					because you know me
+					“ I am lucky, so you are. <br />
+					Because you know me. ”
 				</p>
 			</div>
 		</div>
