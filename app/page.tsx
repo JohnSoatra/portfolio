43cfcd21f416ab84c015460b7d89767fd03a3104
John Soatra <johnsoatra@gmail.com>
2025-01-15 20:09:28 +0700

    update bg

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,27 @@
 import React from "react";
-import Hero from "@/components/home/Hero";
-import Desc from "@/components/home/Bio";
 import Link from "next/link";
 import Image from "next/image";
 import Airplane from "@/components/common/Airplane";
+import Background from "@/components/layout/Background";
 
 const Home = () => {
     return (
         <div className="w-full min-h-screen flex flex-col items-center overflow-hidden">
-            <div className="w-full min-h-full max-w-[1080px] relative flex-1 flex flex-col justify-center items-center pb-[190px]">
-                <div className="background fixed top-0 left-0 w-full h-full opacity-80 blur-[10px]" />
+            <div className="w-full min-h-full max-w-[1080px] relative flex-1 flex flex-col items-center">
+                <Background />
                 <Airplane />
-                <div className="z-10 flex flex-col justify-center items-center gap-y-[10px] rounded-full aspect-square p-[40px]">
-                    {/* <Hero /> */}
-                    <div className="flex flex-col items-center text-center text-[32px] md:text-[42px] xl:text-[52px]">
-                        <h1 className="tracking-wider font-medium ">John Soatra</h1>
-                        {/* <h2 className="tracking-wider">software engineer</h2> */}
-                    </div>
-                    <Desc />
-                    <div className="flex flex-col items-center text-center text-[22px] md:text-[30px] xl:text-[40px]">
-                        {/* <h1 className="tracking-wider font-medium">John Soatra</h1> */}
-                        <h2 className="tracking-wider font-medium">software engineer</h2>
-                    </div>
+                <div className="z-10 flex flex-col items-center mt-[199px]">
+                    <h1 className="tracking-wider font-medium text-center text-[46px] md:text-[60px] xl:text-[83px]">
+                        John Soatra
+                    </h1>
+                    <p className="text-center text-[20px] md:text-[23px] xl:text-[31px] mt-[5px]">
+                        “ I am lucky, so you are. <br />
+                        Because you know me. ”
+                    </p>
+                    <h2 className="tracking-wider self-end text-end text-[15px] mt-[30px]">a programer.</h2>
                     <Link href="https://www.linkedin.com/in/soatra/" className="w-[24px] h-[24px] absolute top-5 left-5 opacity-85" target="_blank">
                         <Image src='/icons/linkedin.png' width={24} height={24} alt="linkedin" />
                     </Link>
-                    {/* <p>I know you need time to think, but now you already spend it all.</p> */}
                 </div>
             </div>
         </div>
---
+++ components/layout/Background.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Background = () => {
+  return (
+    <div className="background fixed top-0 left-0 w-full h-full opacity-80 blur-[10px]" />
+  );
+}
+
+export default Background;
