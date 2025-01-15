c7cd7a20c8e86e523218e08d67cb20ef0d002c4a
Soatra Loma <john.soatra@lomatechnology.com>
2025-01-15 17:46:49 +0700

    change bg

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ const Home = () => {
     return (
         <div className="w-full min-h-screen flex flex-col items-center overflow-hidden">
             <div className="w-full min-h-full max-w-[1080px] relative flex-1 flex flex-col justify-center items-center pb-[190px]">
+                <div className="background fixed top-0 left-0 w-full h-full opacity-80 blur-[10px]" />
                 <Airplane />
                 <div className="z-10 flex flex-col justify-center items-center gap-y-[10px] rounded-full aspect-square p-[40px]">
                     {/* <Hero /> */}
@@ -19,7 +20,7 @@ const Home = () => {
                     <Desc />
                     <div className="flex flex-col items-center text-center text-[22px] md:text-[30px] xl:text-[40px]">
                         {/* <h1 className="tracking-wider font-medium">John Soatra</h1> */}
-                        <h2 className="tracking-wider ">software engineer</h2>
+                        <h2 className="tracking-wider font-medium">software engineer</h2>
                     </div>
                     <Link href="https://www.linkedin.com/in/soatra/" className="w-[24px] h-[24px] absolute top-5 left-5 opacity-85" target="_blank">
                         <Image src='/icons/linkedin.png' width={24} height={24} alt="linkedin" />
