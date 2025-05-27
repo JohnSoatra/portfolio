ec99727ddb2e9c81dbf6ce8397e48aa6f9f6a445
Soatra Loma <john.soatra@lomatechnology.com>
2025-05-27 16:13:03 +0700

    change quote

--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,19 @@ const Home = () => {
         <div className="w-full min-h-screen flex flex-col items-center overflow-hidden">
             <div className="w-full min-h-full max-w-[1080px] relative flex-1 flex flex-col items-center">
                 <Background />
-                <Airplane />
+                {/* <Airplane /> */}
                 <div className="z-10 flex flex-col items-center mt-[199px]">
-                    <h1 className="tracking-wider font-medium text-center text-[46px] md:text-[60px] xl:text-[83px]">
-                        John Soatra
-                    </h1>
-                    <p className="text-center text-[20px] md:text-[23px] xl:text-[31px] mt-[5px]">
-                        “ I am lucky, so you are. <br />
-                        Because you know me. ”
-                    </p>
-                    <h2 className="tracking-wider self-end text-end text-[15px] mt-[30px]">a programer.</h2>
+                    <div className="px-[8px]">
+                        <h1 className="tracking-wider font-medium text-center text-[46px] md:text-[60px] xl:text-[80px]">
+                            John Soatra
+                        </h1>
+                        <p className="text-center text-[18px] md:text-[20px] xl:text-[24px] mt-[8px]">
+                            {/* “ I am lucky, so you are. <br />
+                            Because you know me. ” */}
+                            “ The sun still shines, even when the flowers refuse its light. ”
+                        </p>
+                        <h2 className="tracking-wider self-end text-end text-[14px] mt-[30px] text-[#111]">nobody.</h2>
+                    </div>
                     <Link href="https://www.linkedin.com/in/soatra/" className="w-[24px] h-[24px] absolute top-5 left-5 opacity-85" target="_blank">
                         <Image src='/icons/linkedin.png' width={24} height={24} alt="linkedin" priority />
                     </Link>
