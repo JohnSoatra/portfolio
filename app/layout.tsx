d0df603a6e91be90ffd328907129cae6959ad768
soatra <soatra206.infinity@gmail.com>
2026-04-21 14:43:08 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,43 @@
-import "@/assets/styles/global.scss";
+import "./global.css";
 import type { Metadata, Viewport } from "next";
+import { Roboto } from 'next/font/google'
 import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
-    title: "Soatra",
-    description: `
+  title: "Soatra",
+  description: `
         John Soatra is a creative software engineer that can build very awesome application.
         He's very good at math, so he always embed his knowledge with his creativeness into engineering to make a prefect result.
         He is a kind of person that never give up easily so he is very trustable because when he meets problem,
         he can stay with it all the time however find the best to solve that problem.
         You will like Soatra, if you know him.
     `.trim().replace(/\s+/g, ' '),
-    authors: {
-        name: 'John Soatra',
-        url: 'https://www.soatra.com'
-    },
-    metadataBase: new URL('https://www.soatra.com')
+  authors: {
+    name: 'John Soatra',
+    url: 'https://www.soatra.com',
+  },
+  metadataBase: new URL('https://www.soatra.com'),
 };
 
 export const viewport: Viewport = {
-    themeColor: 'white',
-    colorScheme: 'light'
+  themeColor: 'white',
+  colorScheme: 'light',
 }
 
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700", "900"],
+  variable: "--font-roboto",
+  display: "swap",  
+});
+
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-    return (
-        <html lang="en">
-            <head>
-                <link rel="preload" href='/fonts/Roboto/roboto-regular.woff2' as="font" type="font/woff2" crossOrigin="anonymous"></link>
-                <link rel="preload" href='/fonts/Roboto/roboto-medium.woff2' as="font" type="font/woff2" crossOrigin="anonymous"></link>
-                <link rel="preload" href='/fonts/Roboto/roboto-bold.woff2' as="font" type="font/woff2" crossOrigin="anonymous"></link>
-            </head>
-            <body>
-                {children}
-                <Toaster />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable} font-roboto`}>
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,39 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import Airplane from "@/components/common/Airplane";
 import Background from "@/components/layout/Background";
 
 const Home = () => {
-    return (
-        <div className="w-full min-h-screen flex flex-col items-center overflow-hidden">
-            <div className="w-full min-h-full max-w-[1080px] relative flex-1 flex flex-col items-center">
-                <Background />
-                {/* <Airplane /> */}
-                <div className="z-10 flex flex-col items-center mt-[199px]">
-                    <div className="px-[8px]">
-                        <h1 className="tracking-wider font-medium text-center text-[46px] md:text-[60px] xl:text-[80px]">
-                            John Soatra
-                        </h1>
-                        <p className="text-center text-[18px] md:text-[20px] xl:text-[24px] mt-[8px]">
-                            {/* “ I am lucky, so you are. <br />
-                            Because you know me. ” */}
-                            “ The sun still shines, even when the flowers refuse its light. ”
-                        </p>
-                        <h2 className="tracking-wider self-end text-end text-[14px] mt-[30px] text-[#111]">nobody.</h2>
-                    </div>
-                    <Link href="https://www.linkedin.com/in/soatra/" className="w-[24px] h-[24px] absolute top-5 left-5 opacity-85" target="_blank">
-                        <Image src='/icons/linkedin.png' width={24} height={24} alt="linkedin" priority />
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div className="w-full min-h-screen flex flex-col items-center overflow-hidden">
+      <div className="w-full min-h-full max-w-270 relative flex-1 flex flex-col items-center">
+        <Background />
+        <div className="z-10 flex flex-col items-center mt-49.75">
+          <div className="px-2">
+            <h1 className="tracking-wider font-medium text-center text-[46px] md:text-[60px] xl:text-[80px]">
+              John Soatra
+            </h1>
+            <p className="text-center text-[18px] md:text-[20px] xl:text-[24px] mt-2">
+              “ The sun still shines, even when the flowers refuse its light. ”
+            </p>
+            <h2 className="tracking-wider self-end text-end text-[14px] mt-7.5 text-one">nobody.</h2>
+          </div>
+          <Link
+            href="https://www.linkedin.com/in/soatra/"
+            target="_blank"
+            className="w-6 h-6 absolute top-5 left-5 opacity-85">
+            <Image
+              priority
+              alt="linkedin"
+              src='/icons/linkedin.png'
+              width={24}
+              height={24}
+            />
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Home;
